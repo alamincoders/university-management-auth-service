063bd53cc0621359d2226d422a740bd79fb47fb2
alamincoders <developeralamin03@gmail.com>
2023-05-30 22:31:48 +0600

    git init

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+
+console.log('port')
+
+// create app
+const app: Application = express()
+
+// use middleware
+app.use(cors())
+
+// body parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// create routes
+
+// default route
+app.get('/', (req: Request, res: Response) => {
+  res.send(`Welcome to university management auth service backend`)
+})
+
+export default app
