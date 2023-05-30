3e530c971703659f81cfdcbcbe648f3511cf249a
alamincoders <developeralamin03@gmail.com>
2023-05-30 23:07:11 +0600

    eslint prettier husky lint-staged setup done

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
-import express, { Application, Request } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-
-console.log('port')
 
 // create app
 const app: Application = express()
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-import config from "./config/index";
-import app from "./app";
+import mongoose from 'mongoose'
+import config from './config/index'
+import app from './app'
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(config.mongo_uri as string);
-    console.log("Mongodb Connected!");
+    await mongoose.connect(config.mongo_uri as string)
+    console.log('Mongodb Connected!')
 
     app.listen(config.port, () => {
-      console.log(`Server is running on port ${config.port}`);
-    });
+      console.log(`Server is running on port ${config.port}`)
+    })
   } catch (error: any) {
-    console.log("Failed to connect database", error.message);
+    console.log('Failed to connect database', error.message)
   }
-};
+}
 
-dbConnect();
+dbConnect()
