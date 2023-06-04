e31e4132c88a99c86d6b7272e9896954a57cb2f1
alamincoders <developeralamin03@gmail.com>
2023-06-04 11:28:01 +0600

    winston logger install and setup

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose'
 import config from './config/index'
 import app from './app'
+import logger from './shared/logger'
 
 const dbConnect = async () => {
   try {
     await mongoose.connect(config.mongo_uri as string)
-    console.log('Mongodb Connected!')
+    logger.info('Mongodb Connected!')
 
     app.listen(config.port, () => {
-      console.log(`Server is running on port ${config.port}`)
+      logger.info(`Server is running on port ${config.port}`)
     })
-  } catch (error: any) {
-    console.log('Failed to connect database', error.message)
+  } catch (err) {
+    logger.error('Failed to connect database', err)
   }
 }
 
