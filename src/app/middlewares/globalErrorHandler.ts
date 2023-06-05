453197909ae796164ccedfbfb39aad3d9282601a
alamincoders <developeralamin03@gmail.com>
2023-06-05 11:50:46 +0600

    error handling

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,14 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
+import { IGenericErrorMessage } from '../../interfaces/error';
+import config from '../../config';
 
 const globalErrorHandler = (
-  err: unknown,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  err: any,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.status(400).json({
-    error: err,
+  const statusCode = 500;
+  const message = 'something went wrong!';
+  const errorMessages: IGenericErrorMessage[] = [];
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env !== 'production' ? err?.stack : undefined,
   });
+
   next();
 };
 
--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,7 @@ import path from 'path';
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
+  env: process.env.NODE_ENV,
   port: process.env.PORT,
   mongo_uri: process.env.MONGO_URI,
   default_user_pass: process.env.DEFAULT_USER_PASS,
