f401ec71b332f97c5f91f9915cd1b7e3dae64da4
alamincoders <developeralamin03@gmail.com>
2023-06-08 13:06:42 +0600

    create semester code validation

--- src/app/modules/academicSemester/academicSemester.services.ts
+++ src/app/modules/academicSemester/academicSemester.services.ts
@@ -1,12 +1,19 @@
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
 import { errorLogger } from '../../../shared/logger';
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
 import { IAcademicSemester } from './academicSemester.interfaces';
 import AcademicSemester from './academicSemester.model';
 
 const createSemester = async (
-  params: IAcademicSemester
+  payload: IAcademicSemester
 ): Promise<IAcademicSemester | undefined> => {
   try {
-    const createAcademicSemester = await AcademicSemester.create(params);
+    if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Semester Code !');
+    }
+
+    const createAcademicSemester = await AcademicSemester.create(payload);
     return createAcademicSemester;
   } catch (error) {
     errorLogger.error('Failed to created academic semester!', error);
