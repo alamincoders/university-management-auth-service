4e1ea459e92dd4f56a8ec4cc2e3dfc4d6392c648
alamincoders <developeralamin03@gmail.com>
2023-06-22 16:45:51 +0600

    updated

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,12 +4,11 @@ import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
-  console.log(req.body);
-
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'Login is successfully !',
+    data: req.body,
   });
 });
 
