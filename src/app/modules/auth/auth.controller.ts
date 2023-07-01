944af0954fa4844502872d43f40a4ae05d217d3a
alamincoders <developeralamin03@gmail.com>
2023-07-01 15:26:09 +0600

    jwt access & refresh token with needspasswordchange login successfuly

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -2,13 +2,14 @@ import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
+import { ILoginUserResponse } from './auth.interface';
 import { AuthServices } from './auth.services';
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const { ...loginData } = req.body;
   const result = await AuthServices.loginUser(loginData);
 
-  sendResponse(res, {
+  sendResponse<ILoginUserResponse>(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'Login is successfully !',
